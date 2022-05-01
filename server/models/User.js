06286790a62730299dbf76eec9ae642e76b0ba38
Ahmed Saied <36517568+ahmed-saied-soliman@users.noreply.github.com>
2022-05-01 05:47:11 +0200

    adding user model

---
+++ server/models/User.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema({
+  id: { type: String },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
